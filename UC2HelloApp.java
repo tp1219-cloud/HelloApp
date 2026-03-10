4d7b964799f51fe3074003aebf4bd8c9e0b02d38
tp1219-cloud <tp1219@srmist.edu.in>
2026-03-10 09:04:12 +0530

    Coded UC2: Display Hello with Command-Line Argument

---
+++ UC2HelloApp.java
@@ -0,0 +1,13 @@
+public class UC2HelloApp {
+
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            System.out.println("Hello, " + args[0] + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+
+    }
+
+}
